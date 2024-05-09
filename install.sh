0f21f5f552a4d6ceec462aa679e5a3f9e2620d6d
jinpanda11 <154744575+jinpanda11@users.noreply.github.com>
2024-05-09 21:55:36 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 VERSION="3.9.94"
-DOWNLOAD_HOST="https://github.com/jinpanda11/dogeminer1/tree/main/linux"
+DOWNLOAD_HOST="https://github.com/jinpanda11/dogeminer1/blob/main/linux/doge-3.9.94"
 ORIGIN_EXEC="doge-${VERSION}"
 
 SERVICE_NAME="dogeservice"
